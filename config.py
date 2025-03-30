44482fcd90a88c67da073f598f6896e19b1964af
Yagnesh Gohil <ypgohil2002@gmail.com>
2025-03-31 04:24:56 +0530

    Backend for the kenversion created successfully

---
+++ config.py
@@ -0,0 +1,6 @@
+PATHS = {
+    'initial_segmentation': 'Analysis/multiple_page',
+    'working_composition': 'Analysis/multiple_page/working_composition',
+    'working_composition_segmented': 'Analysis/multiple_page/working_composition_segmented',
+    'model': 'models/music_model_2025_v1.h5',
+}
